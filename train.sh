d1240b8344944e0aaad9d356e1a8da2431937903
Sven Koitka <sven.koitka@fh-dortmund.de>
2017-03-06 16:09:34 +0100

    Upgraded tensorflow slim to tensorflow v1.0.0 and modified training script

--- train.sh
+++ train.sh
@@ -2,22 +2,21 @@
 
 # Variables to modify ---------------------------------------------------------
 DATASET_NAME=ImageCLEFmed2016-ara-autocrop
-
 NUM_RUNS=2
+NUM_CLONES=2
+BATCH_SIZE=32
 
 # -----------------------------------------------------------------------------
 CURRENT_TIME=$(date "+%Y-%m-%d_%H-%M-%S")
-
 SEEDS=(23 42 3349 24474 32023 1131 13271 3204 316 15949)
-
-DATASET_DIR=./datasets/${DATASET_NAME}
-
+DATASET_DIR=datasets/${DATASET_NAME}
 TRAIN_DIR_BASE=models/${DATASET_NAME}/inception-resnet-v2
+PRETRAINED_MODEL_FILE=pretrained/inception_resnet_v2_2016_08_30.ckpt
 
 for((i=0; i<${NUM_RUNS}; i++))
 do
+    # Set training dir for current seed
     CURRENT_SEED=${SEEDS[${i}]}
-
     TRAIN_DIR=${TRAIN_DIR_BASE}/seed_${CURRENT_SEED}
 
     # Fine-tune new layers 5 epoch
@@ -27,14 +26,14 @@ do
         --dataset_name=ImageCLEFmed2016 \
         --dataset_split_name=train \
         --model_name=inception_resnet_v2 \
-        --batch_size=32 \
-        --num_clones=2 \
+        --batch_size=${BATCH_SIZE} \
+        --num_clones=${NUM_CLONES} \
         --max_number_of_steps=1585 \
         --learning_rate=0.0001 \
         --learning_rate_decay_type=fixed \
         --weight_decay=0.0001 \
         --save_summaries_secs=30 \
-        --checkpoint_path=./inception_resnet_v2_2016_08_30.ckpt \
+        --checkpoint_path=${PRETRAINED_MODEL_FILE} \
         --checkpoint_exclude_scopes=InceptionResnetV2/Logits,InceptionResnetV2/AuxLogits \
         --trainable_scopes=InceptionResnetV2/Logits,InceptionResnetV2/AuxLogits \
         --seed=${CURRENT_SEED}
@@ -54,8 +53,8 @@ do
         --dataset_name=ImageCLEFmed2016 \
         --dataset_split_name=train \
         --model_name=inception_resnet_v2 \
-        --batch_size=32 \
-        --num_clones=2 \
+        --batch_size=${BATCH_SIZE} \
+        --num_clones=${NUM_CLONES} \
         --max_number_of_steps=3169 \
         --learning_rate=0.01 \
         --end_learning_rate=0.0001 \
