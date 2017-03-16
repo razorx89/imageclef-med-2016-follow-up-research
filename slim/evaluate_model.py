2afdb74c0381c8aa697c32b3ee1dd6c2e83bc374
Sven Koitka <sven.koitka@fh-dortmund.de>
2017-03-16 11:28:26 +0100

    Added batching to evaluation script for faster inference

--- slim/evaluate_model.py
+++ slim/evaluate_model.py
@@ -1,7 +1,11 @@
+import collections
 import numpy as np
 import os
+from timeit import default_timer as timer
 
 import tensorflow as tf
+from tensorflow.contrib.learn.python.learn.estimators.dynamic_rnn_estimator import _DynamicRNNEstimator
+
 slim = tf.contrib.slim
 from tensorflow.python.platform import tf_logging as logging
 from datasets import dataset_factory, dataset_utils
@@ -18,12 +22,27 @@ tf.app.flags.DEFINE_integer('eval_image_size', None, 'Size of the images')
 tf.app.flags.DEFINE_boolean('oversampling', False, 'Use 10 crops to evaluate one image')
 tf.app.flags.DEFINE_string('output_dir', '.', 'Output directory of result files')
 tf.app.flags.DEFINE_string('output_prefix', None, 'Prefix for each result file name')
+tf.app.flags.DEFINE_integer('batch_size', 100, 'Number of crops to be processed simultaneously')
+tf.app.flags.DEFINE_integer('num_preprocessing_threads', 4, 'The number of threads used to create the batches')
 FLAGS = tf.app.flags.FLAGS
 
 
 def main(_):
     tf.logging.set_verbosity(tf.logging.INFO)
     with tf.Graph().as_default():
+
+        if FLAGS.oversampling:
+            batch_size = max(1, FLAGS.batch_size / 10) * 10
+            images_per_batch = batch_size / 10
+            if batch_size < FLAGS.batch_size:
+                logging.info('Reducing batch size for oversampling strategy')
+        else:
+            batch_size = FLAGS.batch_size
+            images_per_batch = batch_size
+
+        logging.info('Batch size:                 %d', batch_size)
+        logging.info('Number of images per batch: %d', images_per_batch)
+
         ####################################
         # Select dataset and create reader #
         ####################################
@@ -34,8 +53,8 @@ def main(_):
         provider = slim.dataset_data_provider.DatasetDataProvider(dataset,
                                                                   shuffle=False,
                                                                   num_readers=1,
-                                                                  common_queue_capacity=2,
-                                                                  common_queue_min=1)
+                                                                  common_queue_capacity=2*images_per_batch,
+                                                                  common_queue_min=images_per_batch)
         [image, label, name] = provider.get(['image', 'label', 'name'])
 
         num_images = provider.num_samples()
@@ -91,8 +110,15 @@ def main(_):
             crops = tf.expand_dims(preprocess_fn(image, network_input_size, network_input_size), 0)
             images = tf.reshape(crops, [1, network_input_size, network_input_size, 3])
 
-        batch_queue = slim.prefetch_queue.prefetch_queue([images, label, name], capacity=4)
+        images, label, name = tf.train.batch([tf.expand_dims(images, 0), tf.expand_dims(label, 0), tf.expand_dims(name, 0)],
+                                             batch_size=images_per_batch,
+                                             num_threads=FLAGS.num_preprocessing_threads,
+                                             capacity=5*images_per_batch,
+                                             enqueue_many=True)
+
+        batch_queue = slim.prefetch_queue.prefetch_queue([images, label, name], capacity=2)
         images, label, name = batch_queue.dequeue()
+        images = tf.reshape(images, [-1, network_input_size, network_input_size, 3])
 
         #####################
         # Instantiate model #
@@ -132,6 +158,7 @@ def main(_):
             prob_path = os.path.join(FLAGS.output_dir, output_prefix + 'probabilities' + output_suffix + '.csv')
             pred_path = os.path.join(FLAGS.output_dir, output_prefix + 'predictions' + output_suffix + '.csv')
 
+            timings = collections.deque(maxlen=10)
             with open(prob_path, 'w') as ofile_prob, \
                     open(pred_path, 'w') as ofile_pred:
                 # Write header lines to files
@@ -139,31 +166,49 @@ def main(_):
                 ofile_prob.write('name,%s\n' % ','.join([labels[x] for x in sorted(labels.keys())]))
 
                 num_correct = 0
-                for i in range(num_images):
+                processed = 0
+                while processed < num_images:
                     # Execute one step
+                    started = timer()
                     prediction, gt_class, image_name = sess.run([probabilities, label, name])
+                    timings.append(timer() - started)
 
                     # Compute statistics
-                    prediction = np.mean(prediction, axis=0)
-                    predicted_class = np.argmax(prediction)
-                    if predicted_class == gt_class:
-                        num_correct += 1
-
-                    # Get label of image
-                    if labels:
-                        predicted_label = labels[predicted_class]
+                    if FLAGS.oversampling:
+                        prediction = np.reshape(prediction, (images_per_batch, 10, -1))
+                        prediction = np.mean(prediction, axis=1)
+                        predicted_class = np.argmax(prediction, axis=1)
                     else:
-                        predicted_label = '%d' % predicted_class
+                        predicted_class = np.argmax(prediction, axis=1)
 
-                    # Write to output files
-                    ofile_pred.write('%s,%s\n' % (image_name, predicted_label ))
-                    ofile_prob.write('%s,%s\n' % (image_name, ','.join(['%f' % x for x in prediction])))
+                    for i in range(prediction.shape[0]):
+                        if predicted_class[i] == gt_class[i]:
+                            num_correct += 1
 
-                    # Output status info
-                    if (i + 1) % 100 == 0:
-                        logging.info('%d/%d images processed' % (i+1, num_images))
+                        # Get label of image
+                        if labels:
+                            predicted_label = labels[predicted_class[i]]
+                        else:
+                            predicted_label = '%d' % predicted_class[i]
 
-            if (i + 1) % 100 != 0:
+                        # Write to output files
+                        ofile_pred.write('%s,%s\n' % (image_name[i], predicted_label))
+                        ofile_prob.write('%s,%s\n' % (image_name[i], ','.join(['%f' % x for x in prediction[i, :]])))
+
+                        processed += 1
+                        if processed == num_images:
+                            break
+
+                        # Output status info
+                        if (processed + 1) % 100 == 0:
+                            time_per_step = np.mean(np.asarray(timings))
+                            time_per_image = time_per_step / images_per_batch
+                            logging.info('%d/%d images processed (%.4fs/step, %.4fs/image)' % (processed+1,
+                                                                                               num_images,
+                                                                                               time_per_step,
+                                                                                               time_per_image))
+
+            if (processed + 1) % 100 != 0:
                 logging.info('%d/%d images processed' % (num_images, num_images))
 
             logging.info('Accuracy: %f', num_correct / float(num_images))
